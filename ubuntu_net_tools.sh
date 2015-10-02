00d30c77a80aed0f130b8b89513c0c90af394275
Ryan MacLean <ryan@ryanmaclean.com>
2015-10-01 17:31:36 -0700

    Update ubuntu_net_tools.sh

--- ubuntu_net_tools.sh
+++ ubuntu_net_tools.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y gns3 rancid nmap mtr iptraf netcat gns3
+sudo apt-get install -y gns3 rancid nmap mtr iptraf netcat gns3 
+
+#Optional Apps
+#sudo apt-get install -y speedometer netperf nload slurm tcpflow vnstat ipfm 
+
+#Optional Web Apps
+#sudo apt-get install -y ntop
