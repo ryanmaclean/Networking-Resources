876d78b46ffb90c6479972a8a78cf6c002bcdff4
Ryan MacLean <ryan@ryanmaclean.com>
2015-10-12 20:30:19 -0700

    Removed duplicate entry for GNS3
    It's that cool!

--- ubuntu_net_tools.sh
+++ ubuntu_net_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y gns3 rancid nmap mtr iptraf netcat gns3 
+sudo apt-get install -y rancid nmap mtr iptraf netcat gns3 
 
 #Optional Apps
 #sudo apt-get install -y speedometer netperf nload slurm tcpflow vnstat ipfm cbm bwm-ng 
